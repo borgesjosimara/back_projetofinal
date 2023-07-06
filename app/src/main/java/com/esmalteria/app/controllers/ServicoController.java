d5ce79fc3b7ec6772b2f79b2f804218ae5309a66
Amaral <dmrramaral@gmail.com>
2023-07-05 21:57:07 -0300

    CRUD BAck end Criado

--- app/src/main/java/com/esmalteria/app/controllers/ServicoController.java
+++ app/src/main/java/com/esmalteria/app/controllers/ServicoController.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +18,8 @@ import com.esmalteria.app.repository.ServicoRepository;
 import lombok.AllArgsConstructor;
 
 @RestController
-@RequestMapping(value="/servico")
-@AllArgsConstructor	
+@RequestMapping(value = "/api/servicos")
+
 public class ServicoController {
 
 	@Autowired
@@ -25,15 +29,35 @@ public class ServicoController {
 	public List<Servico> listar() {
 		return servicoRepository.findAll();
 	}
-/* 
-	# buscar por id
 
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Servico buscarPorId(Long id) {
+		return servicoRepository.findById(id).get();
+	}
 
-	# cadastrar
+	@PostMapping
+	public Servico cadastrar(@RequestBody Servico servico) {
+		return servicoRepository.save(servico);
+	}
 
-	# deletar */
+	@DeleteMapping(value = "/{id}")
+	public void deletar(@PathVariable Long id) {
+		servicoRepository.deleteById(id);
+	}
 
+	@PostMapping(value = "/{id}")
+	public Servico atualizar(@PathVariable Long id, @RequestBody Servico servico) {
+		servico.setId(id);
+		return servicoRepository.save(servico);
+	}
 
-
+	/*
+	 * # buscar por id
+	 * 
+	 * 
+	 * # cadastrar
+	 * 
+	 * # deletar
+	 */
 
 }
